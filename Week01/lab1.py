20c42f2238f275f0f2ed7acdf4d359d0c3e6b22a
BartyCrouch <onealjean05@gmail.com>
2026-01-16 11:51:28 -0500

    add lab 01 && git push

---
+++ Week01/lab1.py
@@ -0,0 +1,11 @@
+# Sample Coding Question 01 Week 01 
+#O'Neal Jean 
+
+#Question 2: 
+my_array =  [1, 4, 7, 9]
+
+#Question 3: Order of Operations: 
+a = 1 
+b = 2 
+c = 3 
+d = 4
---
+++ Week01/q5.py
@@ -0,0 +1,5 @@
+userAge = int(input("Enter your age: "))
+userAge = userAge + 11 
+print("Now showing the shop items filtered by age", userAge)
+
+#git add . && git commit -m "add lab 01" && git push
